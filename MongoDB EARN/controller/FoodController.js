ca76c3fe37722be232aad4fcf48f937e4723bd6a
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2022-03-28 12:56:34 +0800

    adding order foods users

---
+++ MongoDB EARN/controller/FoodController.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const Food = require("../models/Food");
+
+const get_foods = (req, res, next) => {};
+module.exports = {
+  get_foods
+};
---
+++ MongoDB EARN/routes/api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+
+// Api 
+
+
+
+
+module.exports = router;
